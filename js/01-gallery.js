d3dd8cf14c089fc15c79f6493dc31a64a29a93b2
Alena-007 <alena.tv@ukr.net>
2022-07-17 16:46:11 +0300

    add solution task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from "./gallery-items.js";
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const containerEl = document.querySelector('.gallery');
+
+// console.log(listEl);
+
+function createGalleryMarkup(array) {
+    return array
+        .map(
+            arr => `<div class="gallery__item">
+            <a class="gallery__link" href="${arr.original}">
+            <img class="gallery__image" src="${arr.preview}" data-source="${arr.original}" alt="${arr.description}"/></a></div>`
+        )
+        .join('');
+}
+
+// console.log(createGalleryMarkup(galleryItems));
+
+// containerEl.innerHTML = createGalleryMarkup(galleryItems);
+containerEl.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
+
+containerEl.addEventListener('click', openModal);
+
+function openModal(event) {
+    event.preventDefault();
+
+    const instance = basicLightbox.create(
+        `<img src="${event.target.dataset.source}" width="800" height="600">`,
+        {
+            onShow: () => window.addEventListener('keydown', onClickEsc),
+            onClose: () => window.removeEventListener('keydown', onClickEsc),
+        }
+    );
+    instance.show();
+
+    function onClickEsc(event) {
+        if (event.keyCode === 27) {
+            instance.close();
+        }
+    }
+
+    console.dir(instance);
+}
+
+// console.log(galleryItems);
